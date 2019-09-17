56fab11f744d1c28dff85c13aefc949f6f4ad4d8
Ha Pham <hapham0392@gmail.com>
2019-09-17 22:08:33 +0000

    tweaked

--- eqArrays.js
+++ eqArrays.js
@@ -32,6 +32,6 @@ console.log(eqArrays([1, 2, 3], [1, 2, 3]));// => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
