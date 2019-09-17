c2e8a4d7d03476080a9cb2a84c15602713244b08
Ha Pham <hapham0392@gmail.com>
2019-09-17 21:42:47 +0000

    added  note

---
+++ eqArrays.js
@@ -0,0 +1,37 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const passed = "Assertion Passed: ";
+  const passSticker = "✅ ";
+  const failed = "Assertion Failed: ";
+  const failSticker = "❗️ ";
+  const equal = " === ";
+  const dne = " !== ";
+  if (actual === expected) {
+    console.log(`${passSticker} ${passed}` + actual + `${equal}` +  expected);
+  } else {
+    console.log(`${failSticker} ${failed}` + actual + `${dne}` + expected);
+  }
+};
+
+let eqArrays = function(x, y) {
+  let final = "";
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] === y[i]) {
+      final = "T";
+    } else {
+      final = "F";
+    }
+  } if (final === "T") {
+    return true;
+  } else {
+    return false;
+  }
+};
+// You have to assign true and false to a variable because it will stop the loop if its returned nested inside.
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));// => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
