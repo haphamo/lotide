ba43ec9823d32c7268c524c76b9c08d9dcb55ae3
Ha Pham <hapham0392@gmail.com>
2019-09-18 17:52:18 +0000

    count letters exercise

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const passed = "Assertion Passed: ";
+  const passSticker = "✅ ";
+  const failed = "Assertion Failed: ";
+  const failSticker = "❗️ ";
+  const equal = " === ";
+  const dne = " !== ";
+  if (actual === expected) {
+    console.log(`${passSticker} ${passed}` + actual + `${equal}` +  expected);
+  } else {
+    console.log(`${failSticker} ${failed}` + actual + `${dne}` + expected);
+  }
+};
+
+const countLetters = function(str) {
+  let results = {};
+  for (let letter of str) {
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  } console.log(results);
+  return results;
+};
+countLetters("lighthouse in the house");
