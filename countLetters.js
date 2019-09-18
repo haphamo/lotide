feb3ad1b7d72651af1751afac214590e63bd3dfc
Ha Pham <hapham0392@gmail.com>
2019-09-18 18:55:58 +0000

    Added test code

--- countLetters.js
+++ countLetters.js
@@ -16,6 +16,7 @@ const assertEqual = function(actual, expected) {
 const countLetters = function(str) {
   let results = {};
   for (let letter of str) {
+    
     if (letter !== ' ') {
       if (results[letter]) {
         results[letter] += 1;
@@ -26,4 +27,10 @@ const countLetters = function(str) {
   } console.log(results);
   return results;
 };
+
 countLetters("lighthouse in the house");
+//Test code
+assertEqual(countLetters("lighthouse in the house").l, 1);
+assertEqual(countLetters("lighthouse in the house").h, 4);
+assertEqual(countLetters("lighthouse in the house").e, 3);
+
