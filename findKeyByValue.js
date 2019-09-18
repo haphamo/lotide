04fb16d127db7e0f5b45470c4f27cdac52e47382
Ha Pham <hapham0392@gmail.com>
2019-09-18 20:50:07 +0000

    find key by value in objects

---
+++ findKeyByValue.js
@@ -0,0 +1,38 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const passed = "Assertion Passed: ";
+  const passSticker = "✅ ";
+  const failed = "Assertion Failed: ";
+  const failSticker = "❗️ ";
+  const equal = " === ";
+  const dne = " !== ";
+  if (actual === expected) {
+    console.log(`${passSticker} ${passed}` + actual + `${equal}` +  expected);
+  } else {
+    console.log(`${failSticker} ${failed}` + actual + `${dne}` + expected);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function (bestTVShowsByGenre, tvShow) {
+//use object.keys to generate an array of all the keys
+let genreList = (Object.keys(bestTVShowsByGenre));
+  //console.log(genreList);
+  for (let genre of genreList) {
+    /*if (bestTVShowsByGenre.hasOwnProperty(genre))*/ {
+      if (bestTVShowsByGenre[genre] === tvShow) {
+        return genre;
+     }
+   };
+    console.log(genre);
+  }
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
