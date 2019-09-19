69c11e2602d5e12cc822e0a944af34bfb7e69928
Ha Pham <hapham0392@gmail.com>
2019-09-19 20:22:56 +0000

    created takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,55 @@
+//The function will return a "slice of the array with elements taken from the beginning." It should keep going until the callback/predicate returns a truthy value.
+//Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
+
+//const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+//const results1 = takeUntil(data1, x => x < 0);
+//console.log(results1);
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      return results;
+    } results.push(item);
+  } 
+  return results;
+}
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+// FUNCTION IMPLEMENTATION
+let eqArrays = function(arr1, arr2) {
+  let result = "";
+  if(arr1.length !== arr2.length){
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+    } else {
+      result = false;
+    }
+  } if (result === false) {
+    return false;
+  } else if (result === "") {
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  const passed = "Assertion Passed: ";
+  const passSticker = "✅ ";
+  const failed = "Assertion Failed: ";
+  const failSticker = "❗️ ";
+  const equal = " === ";
+  const dne = " !== ";
+  if (eqArrays(actual, expected)) {
+    console.log(`${passSticker} ${passed}` + actual + `${equal}` +  expected);
+  } else {
+    console.log(`${failSticker} ${failed}` + actual + `${dne}` + expected);
+  }
+};
+//Test code
+//assertArraysEqual(results1, [1,2,5,7,2]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
