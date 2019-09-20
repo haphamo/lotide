73ad75e32a509482b4cf6c1a617a2de84d904db7
Ha Pham <hapham0392@gmail.com>
2019-09-20 00:46:22 +0000

    eslint

--- findKey.js
+++ findKey.js
@@ -14,15 +14,12 @@ const assertEqual = function(actual, expected) {
 //Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
 
 const findKey = function(object, callback) {
-
-  
-  for(let key in object){
-    
+  for (let key in object) {
     if (callback(object[key])) {
       return key;
-    };
-  } 
-}
+    }
+  }
+};
 
 assertEqual(findKey({
   "Blue Hill": { stars: 1 },
@@ -31,4 +28,4 @@ assertEqual(findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), 'noma') // => "noma"
+}, x => x.stars === 2), 'noma'); // => "noma"
